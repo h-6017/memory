db2540993139ca78fb1029532c08a35e1962a99f
Brandon Brown <bbrown_h@dev.bse.edu>
2015-05-12 16:55:37 -0500

    Testing...

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,5 +1,56 @@
+//Test app with strophe and xmpp
+
 var Hello = {
-}
+    connection: null,
+
+    log: function (msg) {
+        $('#log').append("<p>" + msg + "</p>");
+    }
+};
+
 
 $(document).ready(function () {
+    $('#login_dialog').dialog({
+        autoOpen: true,
+        draggable: false,
+        modal: true,
+        title: 'Connect to XMPP',
+
+
+
+buttons: {
+            "connect": function () {
+                $(document).trigger('connect', {
+                    jid: $('#jid').val(),
+                    password: $('#password').val()
+                });
+
+                $('#password').val('');
+                $(this).dialog('close');
+            }
+        }
+    });
+});
+
+$(document).bind('connect', function(ev, data) {
+    var conn = new Strophe.Connection("http://bosh.metajack.im:5280/xmpp-httpbind");
+    conn.connect(data.jid, data.password, function (status) {
+        if (status === Strophe.Status.CONNECTED) {
+            $(document).trigger('connected');
+        } else if (status === Strophe.Status.DISCONNECTED) {
+            $(document).trigger('disconnected');
+        }
+    });
+    Hello.connection = conn;
+});
+
+$(document).bind('connected', function () {
+    //Inform the user
+    Hello.log("connection established");
+});
+
+$(document).bind('disconnected', function () {
+    Hello.log("Connection terminated.");
+    // remove dead connection object
+    hello.connection = null;
 });
